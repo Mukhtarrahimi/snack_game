4ca46738d512530d0997f9af69665d10d437fd7b
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-02 18:11:43 +0430

    const variable code.

---
+++ snack_game.py
@@ -0,0 +1,9 @@
+# ------------ const variable -------------
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPACE_SIZE = 30
+SLOWNESS = 200
+SNACK_COLOR = "yellow"
+FOOD_COLOR = "red"
+score = 0
+direction = "down"
